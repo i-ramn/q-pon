69fe293ec58ab3a266ea92217550bf4d9309e0fc
iramnyonok <sloed95@gmail.com>
2023-02-02 20:33:45 +0300

    fix: fixed inconsistencies

--- build/js/script.js
+++ build/js/script.js
@@ -1,1 +1,63 @@
-"use strict";var burgerMenu=document.getElementById("burger-menu"),overlay=document.getElementById("menu"),adaptiveSearch=document.querySelector(".adaptive-search"),topHead=document.querySelector(".top-head"),headerNavigation=document.querySelector(".header-navigation--container"),menuVisibilityHandler=function(){"close"===burgerMenu.classList.value?(adaptiveSearch.classList.replace("adaptive-search","adaptive-search--visible"),topHead.classList.add("visible"),headerNavigation.classList.add("visible")):(adaptiveSearch.classList.replace("adaptive-search--visible","adaptive-search"),topHead.classList.remove("visible"),headerNavigation.classList.remove("visible"))},navItem=(burgerMenu.addEventListener("click",function(){this.classList.toggle("close"),menuVisibilityHandler()}),window.addEventListener("resize",function(e){e=e.target.innerWidth;menuVisibilityHandler(),769<e&&burgerMenu.classList.remove("close")}),document.querySelectorAll(".header-navigation--inner-container--item")),headerNavOptions=document.querySelector(".header-navigation--options"),ACTIVE_NAV="header-navigation--options--visible",swiper=(navItem.forEach(function(e){}),new Swiper(".mySwiper",{cssMode:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination"},mousewheel:!0,keyboard:!0}));
+const burgerMenu = document.getElementById('burger-menu');
+const overlay = document.getElementById('menu');
+const adaptiveSearch = document.querySelector('.adaptive-search')
+const topHead = document.querySelector('.top-head')
+const headerNavigation = document.querySelector('.header-navigation--container')
+
+const menuVisibilityHandler = () => {
+  if (burgerMenu.classList.value === 'close') {
+    adaptiveSearch.classList.replace('adaptive-search', 'adaptive-search--visible')
+    topHead.classList.add('visible')
+    headerNavigation.classList.add('visible')
+  }
+  else {
+    adaptiveSearch.classList.replace('adaptive-search--visible', 'adaptive-search')
+    topHead.classList.remove('visible')
+    headerNavigation.classList.remove('visible')
+
+  }
+}
+
+burgerMenu.addEventListener('click',function(){
+  this.classList.toggle("close");
+  menuVisibilityHandler()
+});
+
+window.addEventListener("resize", (e) => {
+  const width = e.target.innerWidth;
+  menuVisibilityHandler()
+
+  if (width > 769) {
+    burgerMenu.classList.remove("close");
+  }
+})
+
+const navItem = document.querySelectorAll('.header-navigation--inner-container--item')
+const headerNavOptions = document.querySelector('.header-navigation--options')
+
+const ACTIVE_NAV = 'header-navigation--options--visible'
+
+navItem.forEach(el => {
+
+  //  el.addEventListener("mouseover", () => {
+  //   headerNavOptions.classList.add('header-navigation--options--visible')
+  // })
+  //
+  // el.addEventListener('mouseleave', () => {
+  //   headerNavOptions.classList.remove('header-navigation--options--visible')
+  // })
+})
+
+
+const swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
