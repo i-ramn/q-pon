c013097489f7c2c320234f2169315ab100bf7558
iramnyonok <sloed95@gmail.com>
2023-02-09 04:00:43 +0300

    feat: updated script

--- build/js/script.js
+++ build/js/script.js
@@ -52,11 +52,14 @@ const qPonFilter = document.querySelector('.filter')
 const qPonFilterList = document.querySelector('.filter--list')
 const qPonFilterTitle = document.querySelector('.filter--title')
 const qPonFilterTitleArrow = document.querySelector('.filter--title--arrow-sm')
+const arrowIcon = document.querySelector('.arrow-icon')
+
 
 qPonFilter.addEventListener('click', (event) => {
   qPonFilterList.classList.toggle('filter--list--active')
   qPonFilterTitle.classList.toggle('filter--title--active')
   qPonFilterTitleArrow.classList.toggle('filter--title--arrow-sm--active')
+  arrowIcon.classList.toggle('arrow-icon--active')
 })
 
 
